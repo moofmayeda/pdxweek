4b20ed98f9044164fb3695f28cc0a746853f5f17
moof mayeda <moofmayeda@yahoo.com>
2016-11-14 15:29:17 -0800

    Destroy votes if a restaurant is deleted

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :upvotes,    -> { where up: true }, class_name: "Vote"
   has_many :downvotes,  -> { where up: false }, class_name: "Vote"
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   validates :name, uniqueness: true
 end
