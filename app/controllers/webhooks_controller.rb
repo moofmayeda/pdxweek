7292844f0ec94f1c08a5ed36f8766c685e2b02cb
moof mayeda <moofmayeda@yahoo.com>
2016-11-15 17:01:08 -0800

    Add info keyword to Slack integration to list all commands

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,8 +1,17 @@
 class WebhooksController < ApplicationController
   def slack
     not_found unless params[:token] == ENV['SLACK_TOKEN']
-    text = "Sorry, I didn't get that. Say the name of a restaurant with a +/- to vote, or say 'new' to get the best places you haven't been to yet."
+    text = "Sorry, I didn't get that. Say the name of a restaurant with a +/- to vote, or say 'info' for a list of all keywords."
     case params[:text]
+    when /\binfo\b/i
+      text =  "Available keywords:\n"\
+              "new : restaurants that you haven't been to yet, ranked from best to worst\n"\
+              "history : your voting history\n"\
+              "best : top 3 ranked restaurants\n"\
+              "worst : bottom 3 ranked restaurants\n"\
+              "list : all restaurants listed from best to worst to unrated\n"\
+              "specific restaurant name : shows how many up and downvotes it has\n"\
+              "specific restaurant name with a + or - : adds an up or downvote to that restaurant"
     when /\bnew\b/i
       text = "New spots for #{params[:user_name]}:\n" + ordered_list(Restaurant.top_unrated(params[:user_id]))
     when /\bhistory\b/i
