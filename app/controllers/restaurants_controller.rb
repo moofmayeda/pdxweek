74ad3072d791878db7ed5bbcff6dc74dc741839e
moof mayeda <moofmayeda@yahoo.com>
2016-11-16 15:52:45 -0800

    Change 'hamburger' to 'burger'

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
     @pizza_restaurants = Restaurant.participating('pizza', 2016).includes(:dishes)
-    @burger_restaurants = Restaurant.participating('hamburger', 2016).includes(:dishes)
+    @burger_restaurants = Restaurant.participating('burger', 2016).includes(:dishes)
     render 'index'
   end
 end
