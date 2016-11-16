298f43d30f398b9e040e3ad9e176db39215d3e7f
moof mayeda <moofmayeda@yahoo.com>
2016-11-16 12:21:53 -0800

    Change 'hamburger' to 'burger' because it's cooler

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,6 +2,7 @@ class WebhooksController < ApplicationController
   def slack
     category = params[:trigger_word][1..-2]
     not_found unless params[:token] == ENV['SLACK_TOKEN'] && ['hamburger', 'pizza'].include?(category)
+    category = "burger" if category == "hamburger"
     text = "Sorry, I didn't get that. Say the name of a restaurant with a +/- to vote, or say 'info' for a list of all keywords."
     year = Time.now.year
     case params[:text]
