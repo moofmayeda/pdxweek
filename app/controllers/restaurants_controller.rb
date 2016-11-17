b7dc336fd51b457eada5c86de0bbb25f0bacefb4
moof mayeda <moofmayeda@yahoo.com>
2016-11-17 11:39:46 -0800

    Display pizza, burger, and dumpling week restaurants on home page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,8 @@
 class RestaurantsController < ApplicationController
   def index
-    @pizza_restaurants = Restaurant.participating('pizza', 2016).includes(:dishes)
-    @burger_restaurants = Restaurant.participating('burger', 2016).includes(:dishes)
+    @pizza_restaurants = Restaurant.participating('pizza', 2016)
+    @burger_restaurants = Restaurant.participating('burger', 2016)
+    @dumpling_restaurants = Restaurant.participating('dumpling', 2016)
     render 'index'
   end
 end
